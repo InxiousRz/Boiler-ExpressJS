a32c5f5f39d702dd531fe7f8d0e1d54864df6ce0
RezaFR <rezza.roziks@gmail.com>
2021-04-29 14:02:01 +0700

    fix index_local

--- index_local.js
+++ index_local.js
@@ -4,61 +4,33 @@
 var app = require('./index');
 var http = require('http');
 // var https = require('https'); //HTTPS
- 
- /**
-  * Get port from environment and store in Express.
-  */
- 
+
+
+
+
 // =================================================================================================
 // ENV
 // =================================================================================================
- const PORT = process.env.PORT || 3000;
- app.set('port', port);
- 
- var server = http.createServer(app);
- server.listen(port);
- server.on('error', onError);
- server.on('listening', onListening);
+
+// SET PORT
+const PORT = process.env.NODE_PORT || 3000;
+app.set('port', PORT);
 
 
- function normalizePort(val) {
-     var port = parseInt(val, 10);
+// RUN APP
+const SERVER = http.createServer(app);
+SERVER.listen(PORT);
+SERVER.on('error', onError);
+SERVER.on('listening', onListening);
+
  
-     if (isNaN(port)) {
-         // named pipe
-         return val;
-     }
- 
-     if (port >= 0) {
-         // port number
-         return port;
-     }
- 
-     return false;
- }
- 
- /**
-  * Event listener for HTTP server "error" event.
-  */
- 
- function onError(error) {
-     console.log('Error');
-     try {
-         console.log(error.message)
-     
-     } catch(err){
-         console.log('Error 2 :'+err);
-     }
- }
- 
- /**
-  * Event listener for HTTP server "listening" event.
-  */
- 
- function onListening() {
-     var addr = server.address();
-     var bind = typeof addr === 'string'
-         ? 'pipe ' + addr
-         : 'port ' + addr.port;
-     debug(addr + ' > Listening on > ' + bind);
- }
+// BIND FUNCTION
+function onError(error){
+    console.log('Error');
+    console.log(error.message)
+}
+
+function onListening(){
+    console.log('Listening on PORT : ' + PORT);
+    console.log('SERVER RUN : http://localhost:' + PORT);
+}
